29e398db3f1c6a51d96c9246de940a0c200c9845
nzmpi <108993233+nzmpi@users.noreply.github.com>
2024-09-03 04:50:19 +0400

    fix: fix a bug in Header

--- extension/packages/nextjs/components/Header.tsx.args.mjs
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -1,5 +1,5 @@
 export const menuIconImports = `import { ChevronDoubleUpIcon } from "@heroicons/react/24/outline";`;
-export const menuObject = `{
+export const menuObjects = `{
   label: "ZkTree",
   href: "/ZkTree",
   icon: <ChevronDoubleUpIcon className="h-4 w-4" />,
